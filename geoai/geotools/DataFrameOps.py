502976b79b0160104d54ef150c74339ad3255112
regi <regi.argamosa@gmail.com>
2024-07-17 02:00:05 +0800

    Add initial exploratory data analysis (EDA) for training dataset

--- geoai/geotools/DataFrameOps.py
+++ geoai/geotools/DataFrameOps.py
@@ -1,6 +1,8 @@
 from ydata_profiling import ProfileReport
 import pandas as pd
-from numpy import ndarray
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class DFOperations:
@@ -9,12 +11,22 @@ class DFOperations:
     raster arrays to pandas DataFrames.
     """
 
-    def convert_to_df(self, array: ndarray, column_name: str) -> pd.DataFrame:
+    figure_size = (12, 8)
+
+    @staticmethod
+    def finalize_plot():
+        """
+        Adjusts the layout to be tight and displays the plot.
+        """
+        plt.tight_layout()
+        plt.show()
+
+    def convert_to_df(self, array: np.ndarray, column_name: str) -> pd.DataFrame:
         """
         Convert a raster array to a pandas DataFrame
 
         Args:
-            array (np.ndarray): The input raster array to be converted.
+            array (np.np.ndarray): The input raster array to be converted.
             column_name (str): The name to be used for the DataFrame's column.
 
         Returns:
@@ -56,3 +68,161 @@ class DFOperations:
             pd.DataFrame: A DataFrame with duplicate rows removed.
         """
         return df.drop_duplicates()
+
+    def plot_dual_scatter(self, df: pd.DataFrame, x: str, y: str) -> None:
+        """
+        Create a scatter plot of two variables in a DataFrame.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the data.
+            x (str): The name of the column representing the x-axis variable.
+            y (str): The name of the column representing the y-axis variable.
+
+        Returns:
+            None
+        """
+        plt.figure(figsize=self.figure_size)
+        sns.scatterplot(data=df, x=x, y=y, hue="Class")
+        plt.title("Scatter Plot of NDVI vs NIR")
+        self.finalize_plot()
+        return None
+
+    def make_pairplot(self, df: pd.DataFrame, hue: str) -> None:
+        """
+        Create a pair plot to visualize the relationships between
+        variables in a DataFrame.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the variables
+                               to be plotted.
+            hue (str): The column name in the DataFrame to be used for
+                       coloring the plot.
+
+        Returns:
+            None
+        """
+        plt.figure(figsize=self.figure_size)
+        sns.pairplot(df, hue=hue)
+        self.finalize_plot()
+        return None
+
+    def make_box_plot(self, df: pd.DataFrame, title: str) -> None:
+        """
+        Generate a box plot of values.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the data.
+            x (str): The name of the column representing the x-axis variable.
+            y (str): The name of the column representing the y-axis variable.
+            title (str): The title of the box plot.
+
+        Returns:
+            None
+        """
+        plt.figure(figsize=self.figure_size)
+        df.plot(kind="box", vert=False)
+        plt.title(title)
+        self.finalize_plot()
+        return None
+
+    def make_3d_scatter(
+        self, df: pd.DataFrame, x: str, y: str, z: str, class_name: str
+    ) -> None:
+        """
+        Create a 3D scatter plot using the specified DataFrame and column names.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the data.
+            x (str): The column name for the x-axis.
+            y (str): The column name for the y-axis.
+            z (str): The column name for the z-axis.
+            class_name (str): The column name for the class.
+
+        Returns:
+            None
+        """
+
+        fig = plt.figure(figsize=self.figure_size)
+        ax = fig.add_subplot(111, projection="3d")
+        ax.scatter(
+            df[x],
+            df[y],
+            df[z],
+            c=df[class_name].astype("category").cat.codes,
+            label=df[class_name],
+        )
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
+        ax.set_zlabel(z)
+        plt.title("3D Scatter Plot of {}, {}, {}".format(x, y, z))
+        self.finalize_plot()
+
+    def make_heatmap(self, df: pd.DataFrame, title: str) -> None:
+        """
+        Generate a heatmap of the correlation matrix for the given DataFrame.
+
+        Args:
+            df (pd.DataFrame): The DataFrame for which the heatmap needs to be generated.
+
+        Returns:
+            None
+        """
+        numerical_df = df.select_dtypes(include=[np.number])
+        plt.figure(figsize=self.figure_size)
+        sns.heatmap(numerical_df.corr(), annot=True, cmap="coolwarm")
+        plt.title(title)
+        self.finalize_plot()
+
+    def plot_row(
+        self, row: pd.Series, class_label: str, title_prefix: str = "Spectral Plot"
+    ) -> None:
+        """
+        Generate a line plot for the provided row with the DataFrame's column names as the x-axis.
+
+        Args:
+            row (pd.Series): The row to be plotted.
+            row_label (str or int): The label or index of the row being plotted.
+            title_prefix (str): The prefix for the title of the plot.
+
+        Returns:
+            None
+        """
+        plt.figure(figsize=self.figure_size)
+        plt.plot(row.index, row.values, marker="o", linestyle="-")
+        plt.title(f"{title_prefix} for Class {class_label}")
+        plt.xlabel("Columns")
+        plt.ylabel("Values")
+        plt.xticks(rotation=45)
+        self.finalize_plot()
+
+    def plot_spectral(self, df: pd.DataFrame) -> None:
+        """
+        Plots the spectral signature by class for a given DataFrame.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the spectral data.
+
+        Returns:
+            None
+        """
+        grouped = df.groupby("Class")
+
+        plt.figure(figsize=self.figure_size)
+
+        for class_name, group in grouped:
+            spectral_data = group.iloc[:, :-1]
+            mean_spectral_values = spectral_data.mean()
+            plt.plot(
+                spectral_data.columns,
+                mean_spectral_values,
+                marker="o",
+                linestyle="-",
+                label=f"Class {class_name}",
+            )
+
+        plt.title("Spectral Signature by Class")
+        plt.xlabel("Spectral Bands")
+        plt.ylabel("Spectral Values")
+        plt.xticks(rotation=45)
+        plt.legend()
+        self.finalize_plot()
--- main-eda.py
+++ main-eda.py
@@ -8,10 +8,22 @@ training_df = pd.read_csv("csv_files/training_data.csv")
 print(training_df)
 df_ops = DFOperations()
 # %%
-# GET THE DESCRIPTIVE STATS
+# # VISUALIZE DATASETS
+df_ops.plot_dual_scatter(training_df, "NIR", "BLUE")
+df_ops.make_pairplot(training_df, "Class")
+df_ops.make_box_plot(training_df, "Boxplot")
+df_ops.make_3d_scatter(training_df, "NIR", "BLUE", "NIR", "Class")
+df_ops.make_heatmap(training_df, "Heatmap")
+df_ops.plot_row(training_df.iloc[1, :-2], "builtup")
+df_ops.plot_row(training_df.iloc[2885, :-2], "trees")
+df_ops.plot_row(training_df.iloc[10625, :-2], "water")
+df_ops.plot_spectral(training_df)
+
+# %%
+# VISUALIZE USING PROFILE
 df_ops.generate_profile_report(training_df, "EDA")
-# %%
+
+# # %%
+# REMOVE DUPLICATE ROWS
 no_duplicates = df_ops.remove_duplicate_rows(training_df)
-# %%
-df_ops.generate_profile_report(no_duplicates, "eda_no_duplicates")
-# %%
+no_duplicates.to_csv("csv_files/final_training_df.csv", index=False)
